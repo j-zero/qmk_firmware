7d54496835341de11283ce7ac993c15fb0329e33
Ringe, Johannes <ringe.johannes@mh-hannover.de>
2020-05-14 11:28:15 +0200

    RGB-Pin angepasst

--- keyboards/handwired/datenpirat/numpad/config.h
+++ keyboards/handwired/datenpirat/numpad/config.h
@@ -51,10 +51,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-#define RGB_DI_PIN B6
+#define RGB_DI_PIN F5
 #ifdef RGB_DI_PIN
     #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 3
+    #define RGBLED_NUM 15
     #define RGBLIGHT_HUE_STEP 8
     #define RGBLIGHT_SAT_STEP 8
     #define RGBLIGHT_VAL_STEP 8
