f6709a6d0ee12a9fceb109fcc1249a712bc48e40
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2021-04-24 01:46:41 +0200

    add rshift_home

--- keyboards/kbdfans/kbd75/rev2/rev2.c
+++ keyboards/kbdfans/kbd75/rev2/rev2.c
@@ -1,12 +1,8 @@
 #include "rev2.h"
-<<<<<<< HEAD
-
-
 
 void matrix_init_kb(void) {
   setPinOutput(B2);
   writePinHigh(B2);
   matrix_init_user();
 }
-=======
->>>>>>> 8405d28670c7c8235d0bc4f48b8ab63e472e128e
+
