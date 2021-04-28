8b18253359f5e104251cbe8a23430e4bcf93cc9c
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2021-04-28 20:41:21 +0200

    config.h moved

--- keyboards/kbdfans/kbd75/config.h
+++ keyboards/kbdfans/kbd75/config.h
@@ -4,7 +4,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4B42
-#define MANUFACTURER    datenpirat
+#define MANUFACTURER    qmkbuilder
 #define PRODUCT         KBD75
 
 /* key matrix size */
@@ -39,23 +39,12 @@
 
 #define RGB_DI_PIN E2
 #ifdef RGB_DI_PIN
-
-// #define RGBLIGHT_ANIMATIONS // save some space!
-//#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_BREATHING
-//#define RGBLIGHT_EFFECT_CHRISTMAS
-//#define RGBLIGHT_EFFECT_KNIGHT
-//#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#define RGBLIGHT_EFFECT_RGB_TEST
-//#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//#define RGBLIGHT_EFFECT_TWINKLE
-
+#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 16
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #define RGBLIGHT_SLEEP
 /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+#define RGBLIGHT_SLEEP
 #endif
